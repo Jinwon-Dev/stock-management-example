a14c44ec20ab133b4d62136589925cb60e916a3f
Jinwon-Dev <yjw9424@naver.com>
2023-09-06 15:35:14 +0900

    feat: 재고감소 로직 작성

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,36 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stock {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock(final Long productId, final Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(final Long quantity) {
+
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+}
