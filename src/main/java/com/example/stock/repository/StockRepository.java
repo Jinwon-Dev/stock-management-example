17f6f2cbf79fcac253670b5111f53195b657d640
Jinwon-Dev <yjw9424@naver.com>
2023-09-07 16:27:52 +0900

    feat: PessimisticLock을 이용한 경쟁 조건 해결

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -1,7 +1,22 @@
 package com.example.stock.repository;
 
 import com.example.stock.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    /**
+     * 장점
+     * : 충돌이 빈번히 일어난다면, Optimistic Lock보다 성능이 좋음
+     * : Lock을 통해 Update를 제어하기 때문에, 데이터 정합성이 보장됨
+     * 
+     * 단점
+     * : 별도의 Lock을 잡기 때문에 성능 감소가 있을 수 있음
+     */
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Stock s WHERE s.id = :id")
+    Stock findByIdWithPessimisticLock(final Long id);
 }
--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -4,6 +4,7 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,9 @@ class StockServiceTest {
 
     @Autowired
     private StockService stockService;
+
+    @Autowired
+    private PessimisticLockStockService pessimisticLockStockService;
 
     @Autowired
     private StockRepository stockRepository;
@@ -48,7 +52,8 @@ class StockServiceTest {
     }
 
     @Test
-    public void 동시에_100개의_요청() throws InterruptedException {
+    @DisplayName("Transactional을 사용하지 않고 synchronized 키워드를 사용한 경쟁 조건 해결")
+    public void 동시에_100개의_요청1() throws InterruptedException {
 
         // given
         final int threadCount = 100;
@@ -62,6 +67,35 @@ class StockServiceTest {
             executorService.submit(() -> {
                 try {
                     stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        final Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // then
+        assertThat(stock.getQuantity()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("PessimisticLock을 이용한 경쟁 조건 해결")
+    public void 동시에_100개의_요청2() throws InterruptedException {
+
+        // given
+        final int threadCount = 100;
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(32);
+        final CountDownLatch latch = new CountDownLatch(threadCount); // 다른 스레드에서 실행중인 작업이 완료될 때까지 대기하도록 도와준다.
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+
+            executorService.submit(() -> {
+                try {
+                    pessimisticLockStockService.decrease(1L, 1L);
                 } finally {
                     latch.countDown();
                 }
